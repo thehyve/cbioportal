df0a46745a31036b792a1be8a1365f8be82099ce
Pieter Lukasse <pieterlukasse@gmail.com>
2017-05-24 15:53:37 +0200

    fixed heatmap data parsing; fixed clustering samples

--- portal/src/main/webapp/js/api/cbioportal-datamanager.js
+++ portal/src/main/webapp/js/api/cbioportal-datamanager.js
@@ -1624,17 +1624,20 @@ window.initDatamanager = function (genetic_profile_ids, oql_query, geneset_ids, 
 			//prepare input:
 			var cluster_input = {};
 			for (var i = 0; i < case_ids.length; i++) {
-				//case ids as key:
-				cluster_input[case_ids[i]] = {};
 				//iterate over genetic entities and get the sample data (heatmap data has genetic entityId as key):
 				for (var j = 0; j < heatmapData.length; j++) {
 					var entityId = heatmapData[j].gene || heatmapData[j].geneset_id;
+					var caseId = heatmapData[j].oncoprint_data[i].sample || heatmapData[j].oncoprint_data[i].patient;
 					//small validation/defensive programming:
 					if (!entityId) {
 						throw new Error("Unexpected error during getClusteringOrder: attribute 'gene' or 'geneset_id' not found");
 					}
 					var value = heatmapData[j].oncoprint_data[i].profile_data;
-					cluster_input[case_ids[i]][entityId] = value;
+					if (!cluster_input[caseId]) {
+						//initialize with case ids as key:
+						cluster_input[caseId] = {};
+					}
+					cluster_input[caseId][entityId] = value;
 				}					
 			}
 			//do hierarchical clustering in background:

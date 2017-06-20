f05df46df835f91d4afeaa73f757b8c66b620264
Sander Tan <sandertan@thehyve.nl>
2017-06-20 16:14:36 +0200

    Strip trailing whitespaces in python layer
    Importer, validator, unit tests and system tests

--- core/src/test/scripts/unit_tests_validate_data.py
+++ core/src/test/scripts/unit_tests_validate_data.py
@@ -113,7 +113,7 @@ class PostClinicalDataFileTestCase(DataFileTestCase):
         validateData.DEFINED_SAMPLE_ATTRIBUTES = DEFINED_SAMPLE_ATTRIBUTES
         self.orig_patients_with_samples = validateData.PATIENTS_WITH_SAMPLES
         validateData.PATIENTS_WITH_SAMPLES = PATIENTS_WITH_SAMPLES
-        
+
         # reset all GSVA global variables when starting a test
         self.orig_gsva_sample_ids = validateData.GSVA_SAMPLE_IDS
         validateData.GSVA_SAMPLE_IDS = GSVA_SAMPLE_IDS
@@ -741,7 +741,7 @@ class FeatureWiseValuesTestCase(PostClinicalDataFileTestCase):
             self.assertEqual(record.line_number, expected_line)
             self.assertEqual(record.column_number, 1)
             self.assertIn('NA', record.getMessage())
- 
+
     def test_gsva_range_gsva_scores(self):
         """Test if an error is issued if the score is outside GSVA scoring range"""
         self.logger.setLevel(logging.ERROR)
@@ -765,7 +765,7 @@ class FeatureWiseValuesTestCase(PostClinicalDataFileTestCase):
         self.assertEqual(record.column_number, 2)
         self.assertEqual(record.cause, '1.5')
         return
-         
+
     def test_range_gsva_pvalues(self):
         """Test if an error is issued if the score is outside pvalue range"""
         self.logger.setLevel(logging.ERROR)
@@ -793,11 +793,11 @@ class FeatureWiseValuesTestCase(PostClinicalDataFileTestCase):
     def test_missing_column_gsva(self):
         #Test if an error is issued if the score and pvalue tables do not have same header
         self.logger.setLevel(logging.ERROR)
-        
+
         ### Error should appear when the second file is validated
         record_list1 = self.validate('data_gsva_pvalues_missing_column.txt',
                                     validateData.GsvaPvalueValidator)
-        
+
         record_list2 = self.validate('data_gsva_scores_missing_column.txt',
                                     validateData.GsvaScoreValidator)
         self.assertEqual(len(record_list1), 0)
@@ -818,7 +818,7 @@ class FeatureWiseValuesTestCase(PostClinicalDataFileTestCase):
     def test_missing_row_gsva(self):
         #Test if an error is issued if the score and pvalue table does not have same rownames
         self.logger.setLevel(logging.ERROR)
-        
+
         ### Error should appear when the second file is validated
         record_list1 = self.validate('data_gsva_pvalues_missing_row.txt',
                                     validateData.GsvaPvalueValidator)
@@ -828,12 +828,12 @@ class FeatureWiseValuesTestCase(PostClinicalDataFileTestCase):
         self.assertEqual(len(record_list1), 0)
         self.assertEqual(len(record_list2), 1)
         for record in record_list2:
-            self.assertEqual(record.levelno, logging.ERROR)        
+            self.assertEqual(record.levelno, logging.ERROR)
         self.assertIn('first column', record.getMessage().lower())
         self.assertIn('not equal', record.getMessage().lower())
         return
-#  
-#          
+#
+#
 
     # TODO: test other subclasses of FeatureWiseValidator
 
@@ -857,7 +857,7 @@ class ContinuousValuesTestCase(PostClinicalDataFileTestCase):
         self.assertEqual(record.cause, '')
         record = record_iterator.next()
         self.assertEqual(record.cause, 'Na')
-        
+
 
 class MutationsSpecialCasesTestCase(PostClinicalDataFileTestCase):
 
@@ -1075,9 +1075,9 @@ class MutationsSpecialCasesTestCase(PostClinicalDataFileTestCase):
         # we expect 5 infos: 3 about silent mutations, 2 general info messages:
         self.assertEqual(len(record_list), 5)
         # First 3 INFO messages should be something like: "Validation of line skipped due to cBioPortal's filtering. Filtered types:"
-        for record in record_list[:3]: 
+        for record in record_list[:3]:
             self.assertIn("filtered types", record.getMessage().lower())
-        
+
 
     def test_alternative_notation_for_intergenic_mutation(self):
         """Test alternative 'notation' for intergenic mutations.
@@ -1086,7 +1086,7 @@ class MutationsSpecialCasesTestCase(PostClinicalDataFileTestCase):
         intergenic mutations, and since the Variant_Classification column is
         often invalid, cBioPortal assumes it to mean that and skips it.
         (even if the Entrez column is absent).
-        Here we test whether the 'gene' Unknown / 0 records are skipped 
+        Here we test whether the 'gene' Unknown / 0 records are skipped
         with a warning when Variant_Classification!='IGR'
         """
         # set level according to this test case:
@@ -1097,7 +1097,7 @@ class MutationsSpecialCasesTestCase(PostClinicalDataFileTestCase):
                                             'swissprot_identifier': 'name'})
         # we expect 1 ERROR and 2 WARNINGs :
         self.assertEqual(len(record_list), 3)
-        
+
         # ERROR should be something like: "No Entrez id or gene symbol provided for gene"
         self.assertIn("no entrez gene id or gene symbol provided", record_list[0].getMessage().lower())
         self.assertEqual(record_list[0].levelno, logging.ERROR)

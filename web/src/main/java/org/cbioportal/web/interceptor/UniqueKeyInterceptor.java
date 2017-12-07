cd5f03cc116ca0d311ba664fd5a58cd1e439b040
dionnezaal <dionnezaal@gmail.com>
2017-12-07 14:30:18 +0100

    API for structural variants with one POST endpoint

--- web/src/main/java/org/cbioportal/web/interceptor/UniqueKeyInterceptor.java
+++ web/src/main/java/org/cbioportal/web/interceptor/UniqueKeyInterceptor.java
@@ -11,6 +11,7 @@ import org.cbioportal.model.MutationSpectrum;
 import org.cbioportal.model.NumericGeneMolecularData;
 import org.cbioportal.model.Patient;
 import org.cbioportal.model.Sample;
+import org.cbioportal.model.StructuralVariant;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJacksonValue;
@@ -109,6 +110,11 @@ public class UniqueKeyInterceptor extends AbstractMappingJacksonResponseBodyAdvi
                     sample.setUniqueSampleKey(calculateBase64(sample.getStableId(), sample.getCancerStudyIdentifier()));
                     sample.setUniquePatientKey(calculateBase64(sample.getPatientStableId(), 
                         sample.getCancerStudyIdentifier()));
+                } else if (object instanceof StructuralVariant) {
+                    
+                    StructuralVariant structuralVariant = (StructuralVariant) object;
+                    structuralVariant.setUniqueSampleKey(calculateBase64(structuralVariant.getSampleId(), structuralVariant.getStudyId()));
+                    structuralVariant.setUniquePatientKey(calculateBase64(structuralVariant.getPatientId(), structuralVariant.getStudyId()));
                 }
             }
         }

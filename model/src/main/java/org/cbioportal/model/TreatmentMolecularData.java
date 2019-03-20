eeba45bcf11fb470f974697adc4b8891d2c1fe6d
Pim van Nierop <pim@thehyve.nl>
2019-03-20 16:19:54 +0100

    Add unimplemented method

--- model/src/main/java/org/cbioportal/model/TreatmentMolecularData.java
+++ model/src/main/java/org/cbioportal/model/TreatmentMolecularData.java
@@ -14,4 +14,9 @@ public class TreatmentMolecularData extends MolecularData implements Serializabl
         this.treatmentId = treatmentId;
     }
 
+    @Override
+    public String getStableId() {
+        return getTreatmentId();
+    }
+
 }

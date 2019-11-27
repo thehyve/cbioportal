100d20b7f81ba014893655e291d60d5a175007ff
Pim van Nierop <pim@thehyve.nl>
2019-11-27 09:14:55 +0100

    Impl. oauth2-token based api access

--- core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/GlobalProperties.java
@@ -274,9 +274,6 @@ public class GlobalProperties {
     @Value("${dat.uuid.revoke_other_tokens:true}") // default is true
     public void setDatRevokeOtherTokens(String property) { datRevokeOtherTokens = Boolean.parseBoolean(property);}
 
-    private static String datMethod;
-    @Value("${dat.method:none}") // default is 'none'
-    public void setDatMethod(String property) { datMethod = property;}
 	/*
      * Trim whitespace of url and append / if it does not exist. Return empty
      * string otherwise.
@@ -1182,10 +1179,6 @@ public class GlobalProperties {
         return datRevokeOtherTokens;
     }
 
-    public static String getDatMethod() {
-        return datMethod;
-    }
-    
     public static String getReferenceGenomeName() {
         return portalProperties.getProperty(UCSC_BUILD, DEFAULT_UCSC_BUILD);
     }

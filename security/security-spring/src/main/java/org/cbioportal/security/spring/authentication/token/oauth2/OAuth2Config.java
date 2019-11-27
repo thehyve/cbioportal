0ef40e0dccf22f4831533c36ce71179e9b3862e1
Pim van Nierop <pim@thehyve.nl>
2019-11-27 12:16:36 +0100

    Impl. OAuth2  token data access classes

---
+++ security/security-spring/src/main/java/org/cbioportal/security/spring/authentication/token/oauth2/OAuth2Config.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019 The Hyve B.V.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS
+ * FOR A PARTICULAR PURPOSE. The software and documentation provided hereunder
+ * is on an "as is" basis, and Memorial Sloan-Kettering Cancer Center has no
+ * obligations to provide maintenance, support, updates, enhancements or
+ * modifications. In no event shall Memorial Sloan-Kettering Cancer Center be
+ * liable to any party for direct, indirect, special, incidental or
+ * consequential damages, including lost profits, arising out of the use of this
+ * software and its documentation, even if Memorial Sloan-Kettering Cancer
+ * Center has been advised of the possibility of such damage.
+ */
+
+/*
+ * This file is part of cBioPortal.
+ *
+ * cBioPortal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.cbioportal.security.spring.authentication.token.oauth2;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@Configuration
+@EnableOAuth2Client
+public class OAuth2Config {
+
+    @Value("${dat.oauth2.clientId}")
+    private String clientId;
+
+    @Value("${dat.oauth2.clientSecret}")
+    private String clientSecret;
+
+    @Value("${dat.oauth2.accessTokenUri}")
+    private String accessTokenUri;
+
+    @Value("${dat.oauth2.userAuthorizationUri}")
+    private String userAuthorizationUri;
+
+    @Value("${dat.oauth2.redirectUri}")
+    private String redirectUri;
+
+    @Bean
+    public OAuth2ProtectedResourceDetails oAuth2ResourceDetails() {
+        final AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
+        details.setAccessTokenUri(accessTokenUri);
+        details.setUserAuthorizationUri(userAuthorizationUri);
+        details.setScope(Arrays.asList("offline_access"));
+        details.setPreEstablishedRedirectUri(redirectUri);
+        details.setUseCurrentUri(true);
+        return details;
+    }
+
+}

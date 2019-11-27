458bb0ae905d064a274e8c150bb094c696a5c236
Pim van Nierop <pim@thehyve.nl>
2019-11-27 13:36:17 +0100

    Add placeholders for unit tests for OAuth2 classes

---
+++ security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/oauth2/OAuth2DataAccessTokenServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.cbioportal.security.spring.authentication.token.oauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * OAuth2DataAccessTokenServiceImpl
+ */
+@RunWith(SpringRunner.class)
+public class OAuth2DataAccessTokenServiceImplTest {
+
+    @Test
+    public void isValid() {
+        // TODO: add tests
+    }
+
+    @Test
+    public void getUsername() {
+        // TODO: add tests
+    }
+
+    @Test
+    public void createAuthenticationRequest() {
+        // TODO: add tests
+    }
+
+}
---
+++ security/security-spring/src/test/java/org/cbioportal/security/spring/authentication/token/oauth2/OAuth2TokenAuthenticationProviderTest.java
@@ -0,0 +1,24 @@
+package org.cbioportal.security.spring.authentication.token.oauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * OAuth2TokenAuthenticationProvider
+ */
+// @ContextConfiguration(classes=TokenAuthenticationFilterTestConfiguration.class)
+@RunWith(SpringRunner.class)
+public class OAuth2TokenAuthenticationProviderTest {
+
+    @Test
+    public void testAuthentication() {
+        // TODO: add tests
+    }
+
+    @Test
+    public void extractAuthorities() {
+        // TODO: add tests
+    }
+
+}

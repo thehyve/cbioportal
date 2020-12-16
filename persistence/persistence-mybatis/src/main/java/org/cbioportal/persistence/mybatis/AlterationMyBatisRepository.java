16ef7ab19a466c1bdd368f25fc29731b311eccd3
Ruslan Forostianov <ruslan@thehyve.nl>
2020-12-16 12:26:16 +0100

    Improve performance of `getSampleAlterationCounts` query
    By breaking the query on two:
    - fetching internal sample ids
    - fetch actual sample alteration counts
    
    and pushing sample filtering clause to the subqueries performance increased significantly.
    
    The most time of the original query was spent on creating the temporal table to join 2 tables (`UNION`)
    and aggregating on the temporal table (hence no indexes can be used).
    
    We changed `UNION` to `UNION ALL` as we don't need to deduplicate rows during the union.

--- persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/AlterationMyBatisRepository.java
+++ persistence/persistence-mybatis/src/main/java/org/cbioportal/persistence/mybatis/AlterationMyBatisRepository.java
@@ -30,9 +30,13 @@ public class AlterationMyBatisRepository implements AlterationRepository {
 
         Pair<List<String>, List<String>> caseIdToProfileIdArrays = createCaseIdToProfileIdArrays(molecularProfileCaseIdentifiers);
 
+        List<Integer> internalSampleIds = alterationCountsMapper.getSampleInternalIds(caseIdToProfileIdArrays.getLeft(),
+            caseIdToProfileIdArrays.getRight());
+        if (internalSampleIds.isEmpty()) {
+            return Collections.emptyList();
+        }
         return alterationCountsMapper.getSampleAlterationCounts(
-            caseIdToProfileIdArrays.getLeft(),
-            caseIdToProfileIdArrays.getRight(),
+            internalSampleIds,
             entrezGeneIds,
             createMutationTypeList(mutationEventTypes),
             createCnaTypeList(cnaEventTypes),

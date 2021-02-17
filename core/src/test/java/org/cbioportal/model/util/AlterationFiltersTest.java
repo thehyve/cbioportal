8c8c1a429e6aba769992c0206868521f0fc7afe7
Pim van Nierop <pim@thehyve.nl>
2021-02-17 07:09:38 +0100

    Add Select tests for AlterationFilter and parent classes

--- core/src/test/java/org/cbioportal/model/util/AlterationFiltersTest.java
+++ core/src/test/java/org/cbioportal/model/util/AlterationFiltersTest.java
@@ -1,10 +1,15 @@
 package org.cbioportal.model.util;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.cbioportal.model.AlterationFilter;
+import org.cbioportal.model.CNA;
 import org.cbioportal.model.DriverAnnotationAlterationFilter;
+import org.cbioportal.model.MutationEventType;
 import org.cbioportal.model.MutationStatusAlterationFilter;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class AlterationFiltersTest {
 
@@ -46,4 +51,112 @@ public class AlterationFiltersTest {
         Assert.assertTrue(f.getSelectedTiers().hasAll());
     }
 
+    @Test
+    public void setSelectAllWhenAllCnaTypesTrue() {
+        AlterationFilter f = new AlterationFilter();
+        Map<CNA, Boolean> types = new HashedMap();
+        types.put(CNA.HOMDEL, true);
+        types.put(CNA.DIPLOID, true);
+        f.setCopyNumberAlterationEventTypes(types);
+        Assert.assertTrue(f.getCnaTypeSelect().hasValues());
+        Assert.assertTrue(f.getCnaTypeSelect().hasAll());
+        Assert.assertFalse(f.getCnaTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenSomeCnaTypesTrue() {
+        AlterationFilter f = new AlterationFilter();
+        Map<CNA, Boolean> types = new HashedMap();
+        types.put(CNA.HOMDEL, false);
+        types.put(CNA.DIPLOID, true);
+        f.setCopyNumberAlterationEventTypes(types);
+        Assert.assertTrue(f.getCnaTypeSelect().hasValues());
+        Assert.assertFalse(f.getCnaTypeSelect().hasAll());
+        Assert.assertFalse(f.getCnaTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenAllCnaTypesFalse() {
+        AlterationFilter f = new AlterationFilter();
+        Map<CNA, Boolean> types = new HashedMap();
+        types.put(CNA.HOMDEL, false);
+        types.put(CNA.DIPLOID, false);
+        f.setCopyNumberAlterationEventTypes(types);
+        Assert.assertFalse(f.getCnaTypeSelect().hasValues());
+        Assert.assertFalse(f.getCnaTypeSelect().hasAll());
+        Assert.assertTrue(f.getCnaTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenAllMutationTypesTrue() {
+        AlterationFilter f = new AlterationFilter();
+        Map<MutationEventType, Boolean> types = new HashedMap();
+        types.put(MutationEventType.feature_truncation, true);
+        types.put(MutationEventType.missense_mutation, true);
+        f.setMutationEventTypes(types);
+        Assert.assertTrue(f.getMutationTypeSelect().hasValues());
+        Assert.assertTrue(f.getMutationTypeSelect().hasAll());
+        Assert.assertFalse(f.getMutationTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenSomeMutationTypesTrue() {
+        AlterationFilter f = new AlterationFilter();
+        Map<MutationEventType, Boolean> types = new HashedMap();
+        types.put(MutationEventType.feature_truncation, false);
+        types.put(MutationEventType.missense_mutation, true);
+        f.setMutationEventTypes(types);
+        Assert.assertTrue(f.getMutationTypeSelect().hasValues());
+        Assert.assertFalse(f.getMutationTypeSelect().hasAll());
+        Assert.assertFalse(f.getMutationTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenAllMutationTypesFalse() {
+        AlterationFilter f = new AlterationFilter();
+        Map<MutationEventType, Boolean> types = new HashedMap();
+        types.put(MutationEventType.feature_truncation, false);
+        types.put(MutationEventType.missense_mutation, false);
+        f.setMutationEventTypes(types);
+        Assert.assertFalse(f.getMutationTypeSelect().hasValues());
+        Assert.assertFalse(f.getMutationTypeSelect().hasAll());
+        Assert.assertTrue(f.getMutationTypeSelect().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenAllTiersTrue() {
+        DriverAnnotationAlterationFilter f = new DriverAnnotationAlterationFilter();
+        Map<String, Boolean> types = new HashedMap();
+        types.put("Class 1", true);
+        types.put("Class 2", true);
+        f.setSelectedTiers(types);
+        Assert.assertTrue(f.getSelectedTiers().hasValues());
+        Assert.assertTrue(f.getSelectedTiers().hasAll());
+        Assert.assertFalse(f.getSelectedTiers().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenSomeTiersTrue() {
+        DriverAnnotationAlterationFilter f = new DriverAnnotationAlterationFilter();
+        Map<String, Boolean> types = new HashedMap();
+        types.put("Class 1", false);
+        types.put("Class 2", true);
+        f.setSelectedTiers(types);
+        Assert.assertTrue(f.getSelectedTiers().hasValues());
+        Assert.assertFalse(f.getSelectedTiers().hasAll());
+        Assert.assertFalse(f.getSelectedTiers().hasNone());
+    }
+
+    @Test
+    public void setSelectAllWhenAllTiersFalse() {
+        DriverAnnotationAlterationFilter f = new DriverAnnotationAlterationFilter();
+        Map<String, Boolean> types = new HashedMap();
+        types.put("Class 1", false);
+        types.put("Class 2", false);
+        f.setSelectedTiers(types);
+        Assert.assertFalse(f.getSelectedTiers().hasValues());
+        Assert.assertFalse(f.getSelectedTiers().hasAll());
+        Assert.assertTrue(f.getSelectedTiers().hasNone());
+    }
+    
 }
